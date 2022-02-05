e14ab56699a4cce19037f2395d9dda5b0869d184
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-05 12:52:35 +0100

    Added hosts file setup

--- cluster/multipass/kubeadm/scripts/init-master.sh
+++ cluster/multipass/kubeadm/scripts/init-master.sh
@@ -2,6 +2,10 @@ set -e
 
 K8S_VERSION=${1:-1.18.9}
 
+IP=$(ip -o -4 addr show dev ens3 | awk '{split($4,a,"/");print a[1]}')
+sudo sed -i s/.*$HOSTNAME.*//g /etc/hosts
+echo "$IP $HOSTNAME" | sudo tee -a /etc/hosts
+
 sudo kubeadm config images pull
 sudo kubeadm init --kubernetes-version ${K8S_VERSION} --token ppozut.y9dh2r1bdowfay3x --pod-network-cidr=10.244.0.0/16 --service-cidr=10.96.0.0/16 --apiserver-advertise-address 192.168.56.4
 

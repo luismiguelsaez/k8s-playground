053032d58af1e67285fd98f0838e17855d2be0cd
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-05 14:01:58 +0100

    Finished inti script

--- cluster/multipass/kubeadm/init.sh
+++ cluster/multipass/kubeadm/init.sh
@@ -2,33 +2,56 @@
 
 CONTROL_NUM=1
 WORKERS_NUM=2
-NODES_NAME_PREFFIX="k3s-kubeadm"
+
+NODES_NAME_PREFFIX="kubeadm"
+NODES_CONTROL_NAME_PREFFIX="control"
+NODES_WORKERS_NAME_PREFFIX="worker"
+
+OS_VERS=20.4
+K8S_VERS=1.18.9
 
 for N in $(seq $CONTROL_NUM)
 do
-    NODE_STATUS=$( multipass list | grep ${NODES_NAME_PREFFIX}-${N} )
+    NODE_STATUS=$( multipass list | grep ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} )
 
     if [ -z "$NODE_STATUS" ]
     then
-        echo -e "\e[32mCreating node ${NODES_NAME_PREFFIX}-${N} ...\e[0m"
-        multipass launch -n ${NODES_NAME_PREFFIX}-${N} -c 2 -m 2048M >/dev/null 2>&1
+        echo -e "\e[32mCreating control node ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} ...\e[0m"
+        multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} -c 2 -m 2048M  -d 20G --cloud-init cloud-config.yaml >/dev/null 2>&1
+
+        multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}:/scripts
+        multipass exec ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} bash /scripts/init-master.sh ${K8S_VERS}
     else
         if [ "$( echo $NODE_STATUS | awk '{print $2;}' )" == "Stopped" ]
         then
-            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${N} already exists. Starting ...\e[32m"
-            multipass start ${NODES_NAME_PREFFIX}-${N} >/dev/null 2>&1
+            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} already exists. Starting ...\e[32m"
+            multipass start ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} >/dev/null 2>&1
         else
-            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${N} already exists\e[0m"
+            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} already exists\e[0m"
         fi
     fi
 done
 
-multipass launch 20.04 -n kubeadm-control-01 -c 2 -m 2048M -d 20G --cloud-init cloud-config.yaml
-multipass mount scripts kubeadm-control-01:/scripts
-multipass exec kubeadm-control-01 bash /scripts/init-master.sh 1.18.9
-
 MASTER_IP=$(multipass info kubeadm-control-01 | awk '/^IPv4/{print $2}')
 
-multipass launch 20.04 -n kubeadm-worker-01 -c 1 -m 1024M -d 20G --cloud-init cloud-config.yaml
-multipass mount scripts kubeadm-worker-01:/scripts
-multipass exec kubeadm-worker-01 bash /scripts/init-worker.sh ${MASTER_IP}:6443
+for N in $(seq $WORKERS_NUM)
+do
+    NODE_STATUS=$( multipass list | grep ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} )
+
+    if [ -z "$NODE_STATUS" ]
+    then
+        echo -e "\e[32mCreating control node ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} ...\e[0m"
+        multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} -c 1 -m 1024M  -d 20G --cloud-init cloud-config.yaml >/dev/null 2>&1
+
+        multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}:/scripts
+        multipass exec ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} bash /scripts/init-worker.sh ${MASTER_IP}
+    else
+        if [ "$( echo $NODE_STATUS | awk '{print $2;}' )" == "Stopped" ]
+        then
+            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} already exists. Starting ...\e[32m"
+            multipass start ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} >/dev/null 2>&1
+        else
+            echo -e "\e[32mNode ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} already exists\e[0m"
+        fi
+    fi
+done

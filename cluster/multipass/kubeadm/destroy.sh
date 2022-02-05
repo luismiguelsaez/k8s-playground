ff07719befe1c95a142ad7ea7695f31fa5eb0f08
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-05 15:58:26 +0100

    Added delete script

---
+++ cluster/multipass/kubeadm/destroy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+CONTROL_NUM=1
+WORKERS_NUM=2
+
+NODES_NAME_PREFFIX="kubeadm"
+NODES_CONTROL_NAME_PREFFIX="control"
+NODES_WORKERS_NAME_PREFFIX="worker"
+
+OS_VERS=20.4
+K8S_VERS=1.18.9
+
+for N in $(seq $CONTROL_NUM)
+do
+    multipass delete ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}
+done
+
+for N in $(seq $WORKERS_NUM)
+do
+    multipass delete ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}
+done
+
+multipass purge

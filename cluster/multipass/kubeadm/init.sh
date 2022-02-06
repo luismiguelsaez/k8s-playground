b762c55e9f663b7ed3112eff941907751525e57a
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-06 08:39:27 +0100

    Fixed outputs

--- cluster/multipass/kubeadm/init.sh
+++ cluster/multipass/kubeadm/init.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 
-CONTROL_NUM=1
-WORKERS_NUM=2
+CONTROL_NUM=${CONTROL_NUM:-1}
+WORKERS_NUM=${WORKERS_NUM:-2}
+
+OS_VERS=${OS_VERS:-20.04}
+K8S_VERS=${K8S_VERS:-1.19.16}
 
 NODES_NAME_PREFFIX="kubeadm"
 NODES_CONTROL_NAME_PREFFIX="control"
 NODES_WORKERS_NAME_PREFFIX="worker"
 
-OS_VERS=20.04
-K8S_VERS=1.18.9
 
 for N in $(seq $CONTROL_NUM)
 do
@@ -18,31 +19,33 @@ do
     if [ -z "$NODE_STATUS" ]
     then
         echo -ne "\e[37mCreating control node \e[34m${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}\e[0m ... \e[0m"
-        multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} -c 2 -m 2048M  -d 20G --cloud-init cloud-config.yaml >/dev/null 2>&1
+        LAUNCH_OUT=$( multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} -c 2 -m 2048M  -d 20G --cloud-init cloud-config.yaml 2>&1 )
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
+            echo "${LAUNCH_OUT}"
         fi
 
         echo -ne "\e[37mMounting scripts in control node ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} ... \e[0m"
-        multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}:/scripts >/dev/null 2>&1
+        MOUNT_OUT=$( multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N}:/scripts 2>&1 )
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
+            echo "${MOUNT_OUT}"
         fi
 
         echo -ne "\e[37mProvisioning control node ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} ... \e[0m"
-        PROVISION_OUT=$(multipass exec ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} bash /scripts/init-master.sh ${K8S_VERS} 2>&1 )
+        PROVISION_OUT=$( multipass exec ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} -- bash /scripts/init-master.sh ${K8S_VERS} 2>&1 )
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
-            echo ${PROVISION_OUT}
+            echo "${PROVISION_OUT}" | bat -l bash
         fi
     else
         if [ "$( echo $NODE_STATUS | awk '{print $2;}' )" == "Stopped" ]
@@ -64,31 +67,33 @@ do
     if [ -z "$NODE_STATUS" ]
     then
         echo -ne "\e[37mCreating worker node \e[35m${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N}\e[0m ... \e[0m"
-        multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} -c 1 -m 1024M  -d 20G --cloud-init cloud-config.yaml >/dev/null 2>&1
+        LAUNCH_OUT=$( multipass launch ${OS_VERS} -n ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} -c 1 -m 1024M  -d 20G --cloud-init cloud-config.yaml 2>&1 )
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
+            echo "${LAUNCH_OUT}"
         fi
 
         echo -ne "\e[37mMounting scripts in worker node ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} ... \e[0m"
-        multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N}:/scripts  >/dev/null 2>&1
+        MOUNT_OUT=$( multipass mount scripts ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N}:/scripts 2>&1 )
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
+            echo "${MOUNT_OUT}"
         fi
 
         echo -ne "\e[37mProvisioning worker node ${NODES_NAME_PREFFIX}-${NODES_CONTROL_NAME_PREFFIX}-${N} ... \e[0m"
-        PROVISION_OUT=$(multipass exec ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} bash /scripts/init-worker.sh ${MASTER_IP}:6443 2>&1)
+        PROVISION_OUT=$(multipass exec ${NODES_NAME_PREFFIX}-${NODES_WORKERS_NAME_PREFFIX}-${N} -- bash /scripts/init-worker.sh ${MASTER_IP}:6443 2>&1)
         if [ $? -eq 0 ]
         then
             echo -e "\e[32mOK\e[0m"
         else
             echo -e "\e[31mERROR\e[0m"
-            echo ${PROVISION_OUT}
+            echo "${PROVISION_OUT}" | bat -l bash
         fi
     else
         if [ "$( echo $NODE_STATUS | awk '{print $2;}' )" == "Stopped" ]
--- cluster/multipass/test.sh
+++ cluster/multipass/test.sh
@@ -1,3 +1,5 @@
-k create deploy nginx --image=nginx:1.20.1-alpine --replicas=6 --port=80
-k expose deploy nginx --name=nginx --port=8080 --target-port=80
-k run test-nginx --image=busybox --restart=Never --rm -it --command -- wget --server-response --timeout=2 http://nginx.default.svc.cluster.local:8080
+kubectl create deploy nginx --image=nginx:1.20.1-alpine --replicas=6 --port=80
+kubectl expose deploy nginx --type=ClusterIP --name=nginx-cip --port=8080 --target-port=80
+kubectl expose deploy nginx --type=NodePort --name=nginx-np --port=8080 --target-port=80
+
+kubectl run test-nginx --image=busybox --restart=Never --rm -it --command -- wget -O- --server-response --timeout=2 http://nginx-cip.default.svc.cluster.local:8080

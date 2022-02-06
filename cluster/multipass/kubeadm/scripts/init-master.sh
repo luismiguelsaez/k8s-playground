f619b317f71d5fd8911e88d3d83f01028ea04c6b
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-06 11:16:29 +0100

    Added metricserver patching

--- cluster/multipass/kubeadm/scripts/init-master.sh
+++ cluster/multipass/kubeadm/scripts/init-master.sh
@@ -23,3 +23,5 @@ sudo chmod +x /usr/local/bin/*
 
 # Install metrics server
 #curl -sL https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml | yq 'select(.kind == "Deployment")|.spec.template.spec.containers[0].args += "--kubelet-insecure-tls"'
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deploy -n kube-system metrics-server --type=json --patch='[{"op":"replace","path":"/spec/template/spec/containers/0/args","value":["--cert-dir=/tmp","--secure-port=4443","--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname","--kubelet-use-node-status-port","--metric-resolution=15s","--kubelet-insecure-tls"]}]'

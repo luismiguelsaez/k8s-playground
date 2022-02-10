36014162f680f82305826d32dbb82354507fa990
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-10 22:17:39 +0100

    Added metric server

--- cluster/vagrant/scripts/init-master.sh
+++ cluster/vagrant/scripts/init-master.sh
@@ -38,6 +38,11 @@ chmod +x /usr/local/bin/etcdctl
 # Calico
 su - vagrant -c "kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml"
 
+# Install metrics server
+#curl -sL https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml | yq 'select(.kind == "Deployment")|.spec.template.spec.containers[0].args += "--kubelet-insecure-tls"'
+su - vagrant -c "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+su - vagrant -c "kubectl patch deploy -n kube-system metrics-server --type=json --patch='[{\"op\":\"replace\",\"path\":\"/spec/template/spec/containers/0/args\",\"value\":[\"--cert-dir=/tmp\",\"--secure-port=4443\",\"--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname\",\"--kubelet-use-node-status-port\",\"--metric-resolution=15s\",\"--kubelet-insecure-tls\"]}]'"
+
 # kubeadm token create --print-join-command
 # Fix wrong internal ip
 # /etc/systemd/system/kubelet.service.d/10-kubeadm.conf

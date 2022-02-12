39c198c1b79b2eff7767cbd3a79027f0cd889e60
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-02-12 15:08:39 +0100

    Re-ordered steps

--- cluster/vagrant/scripts/common.sh
+++ cluster/vagrant/scripts/common.sh
@@ -4,33 +4,23 @@ set -e
 
 echo "Installing K8s packages [${HOSTNAME}:${NODE_IP}]: ${K8S_VERSION}"
 
-apt-get update -y
-apt-get install -y apt-transport-https ca-certificates curl
-
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get update -y
-apt-get install -y --allow-change-held-packages containerd kubelet=${K8S_VERSION}-00 kubeadm=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00
-apt-mark hold kubelet kubeadm kubectl
-
+# Configure system
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 swapoff -a
 
+cat << EOF > /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
 modprobe br_netfilter
-echo br_netfilter >> /etc/modules-load.d/modules.conf
+modprobe overlay
 
 cat << EOF > /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables=1
 net.bridge.bridge-nf-call-iptables=1 
 net.ipv4.ip_forward=1                
 EOF
-
 sysctl --system
-
-systemctl enable kubelet
-
-#ADDR=$(ip -o -4 address show dev enp0s8 | awk '{split($4,a,"/");print a[1]}')
-echo "KUBELET_EXTRA_ARGS='--node-ip=${NODE_IP} --network-plugin=cni'" > /etc/default/kubelet
 
 sed -i s/.*${HOSTNAME}.*//g /etc/hosts
 cat << EOF >> /etc/hosts
@@ -46,3 +36,22 @@ timeout: 2
 debug: false
 pull-image-on-create: false
 EOF
+
+# Configure containerd
+#mkdir /etc/containerd
+#containerd config default | sed 's/\(SystemdCgroup = \).*/\1true/g' > /etc/containerd/config.toml
+
+# Install packages
+apt-get update -y
+apt-get install -y apt-transport-https ca-certificates curl
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update -y
+apt-get install -y --allow-change-held-packages containerd kubelet=${K8S_VERSION}-00 kubeadm=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00
+apt-mark hold kubelet kubeadm kubectl
+
+
+systemctl enable kubelet
+
+echo "KUBELET_EXTRA_ARGS='--node-ip=${NODE_IP} --network-plugin=cni'" > /etc/default/kubelet
